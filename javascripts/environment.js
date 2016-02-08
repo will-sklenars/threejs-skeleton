5997cd8d9704cff9d44aa50887e81a1908b6450f
will-sklenars <will.sklenars@gmail.com>
2016-02-08 14:39:07 +1300

    changed default stats setting

--- javascripts/environment.js
+++ javascripts/environment.js
@@ -50,7 +50,7 @@ $(document).on('ready', function () {
 
     var stats = new Stats();
 
-    stats.setMode( 1 ); // 0: fps, 1: ms, 2: mb
+    stats.setMode( 0 ); // 0: fps, 1: ms, 2: mb
 
     // align top-left
     stats.domElement.style.position = 'absolute';
