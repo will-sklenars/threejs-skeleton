c50a993b367311191a0c9c98e181b05870ddca85
will-sklenars <will.sklenars@gmail.com>
2016-03-01 15:49:55 +1300

    new layout

--- javascripts/environment.js
+++ javascripts/environment.js
@@ -1,105 +1,33 @@
 $(document).on('ready', function () {
 
-  var environment = {}
-  environment.container = undefined
-  environment.camera = undefined
-  environment.controls = undefined
-  environment.scene = undefined
-  environment.renderer  = undefined
-  environment.onRenderFcts = []
-  environment.dollyOut = true
-
-  environment.init = function (opts) {
-
-    var self = this
-
+  function Environment () {
     this.container = document.getElementById( "container" );
-
-
-    /////////////////////////// set up camera /////////////////////////////
-
-    this.camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 0.0001, 10000 );
-    this.camera.position.set(0,0,10)
-
-
-    /////////////////////////// set up controls /////////////////////////////
-
-    this.controls = new THREE.OrbitControls( this.camera, this.container );
-    this.controls.zoomSpeed = 0.2
-    this.controls.mouseButtons = { ORBIT: THREE.MOUSE.LEFT, ZOOM: THREE.MOUSE.MIDDLE };
-
-    this.onRenderFcts.push(this.controls.update)
-
-    /////////////////////////// set up scene /////////////////////////////
-
     this.scene = new THREE.Scene();
+    this.onRenderFcts = []
+  }
 
-    /////////////////////////// set up renderer /////////////////////////////
-
-    this.renderer = new THREE.WebGLRenderer( { antialias: true } );
-    this.renderer.setClearColor( 0xffffff );
-    this.renderer.setPixelRatio( window.devicePixelRatio );
-    this.renderer.setSize( window.innerWidth, window.innerHeight );
-    this.container.appendChild( this.renderer.domElement );
-
-    ///////////////////// On Window Resize ////////////////////////
-
-    var windowResize = new THREEx.WindowResize(this.renderer, this.camera)
-
-    ///////////////////////////////////// Stats ////////////////////////////////////////
-
-    var stats = new Stats();
-
-    stats.setMode( 0 ); // 0: fps, 1: ms, 2: mb
-
-    // align top-left
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.left = '0px';
-    stats.domElement.style.top = '0px';
-    document.body.appendChild( stats.domElement );
-    var $stats = $(stats.domElement)
-
-    $stats.hide()
-
-    this.onRenderFcts.push(function () {
-      stats.begin();
-      stats.end();
-    })
-
-    var rendererStats = new THREEx.RendererStats()
-
-    rendererStats.domElement.style.position = 'absolute'
-    rendererStats.domElement.style.right = '0px'
-    rendererStats.domElement.style.top   = '0px'
-    document.body.appendChild( rendererStats.domElement )
-
-    var $rendererStats = $(rendererStats.domElement)
-    $rendererStats.hide()
-
-
-    this.onRenderFcts.push(function () {
-      rendererStats.update(self.renderer);
-    })
-
-    $(document).on('keypress', function (e) {
-      if ( e.keyCode == 115 || e.keyCode == 83) {
-        $stats.toggle()
-        $rendererStats.toggle()
-      }
-    })
-
-
-    //////////////////////////////////////////////////////////////////////////////
-    //                         render the scene                                 //
-    //////////////////////////////////////////////////////////////////////////////
-
+  Environment.prototype.init = function () {
+    var self = this
+    /////////////////////////// set up camera ///////////////////////////////////////
+    this.initializeCamera()
+    /////////////////////////// set up controls /////////////////////////////////////
+    this.initializeControls()
+    /////////////////////////// set up renderer /////////////////////////////////////
+    this.initializeRenderer()
+    ///////////////////// On Window Resize //////////////////////////////////////////
+    this.initWindowResize()
+    ///////////////////////////////////// Dom Events ////////////////////////////////
+    this.domEvents = new THREEx.DomEvents(this.camera, this.renderer.domElement)
+    ///////////////////////////////////// Stats /////////////////////////////////////
+    this.initStats()
+    this.initRendererStats()
+    /////////////////////// render the scene ////////////////////////////////////////
     this.onRenderFcts.push(function(){
       self.renderer.render( self.scene, self.camera );
     })
-
   }
 
-  environment.populateScene = function () {
+  Environment.prototype.populateScene = function () {
     this.knot = {}
     var geometry = new THREE.TorusKnotGeometry(5, 1, 64, 8, 2, 3, 1);
     var material = new THREE.MeshNormalMaterial({shading: THREE.FlatShading});
@@ -107,10 +35,10 @@ $(document).on('ready', function () {
     this.addObjectToScene(this.knot);
   }
 
-  environment.animate = function () {
+  Environment.prototype.animate = function () {
   }
 
-  environment.render = function () {
+  Environment.prototype.render = function () {
 
     var self = this
     var lastTimeMsec = null
@@ -136,43 +64,129 @@ $(document).on('ready', function () {
   }
 
 
+  /////////////////////////////////////////////////////////////////////
+  ///////////////////////////// init fxns /////////////////////////////
+  /////////////////////////////////////////////////////////////////////
+
+
+  Environment.prototype.initializeCamera = function () {
+    this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.0001, 10000 );
+    this.camera.position.set(0,0,-20)
+  }
+
+  Environment.prototype.initializeControls = function () {
+    this.controls = new THREE.OrbitControls( this.camera, this.container );
+    // this.controls.maxDistance = 5
+    this.controls.minDistance = 1.7
+    this.controls.zoomSpeed = 0.2
+    this.controls.target.set(1,1,1)
+    this.controls.mouseButtons = { ORBIT: THREE.MOUSE.LEFT, ZOOM: THREE.MOUSE.MIDDLE };
+    this.controls.enableKeys = false
+
+    this.onRenderFcts.push(this.controls.update)
+  }
+
+  Environment.prototype.initializeRenderer = function () {
+    var self = this
+
+    this.renderer = new THREE.WebGLRenderer( { antialias: true } );
+    this.renderer.setClearColor( 0x222628 );
+    this.renderer.setPixelRatio( window.devicePixelRatio );
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    this.renderer.sortObjects = false;
+    this.renderer.rendering = true
+    this.container.appendChild( this.renderer.domElement );
+  }
+
+  Environment.prototype.initWindowResize = function () {
+    this.windowResize = new THREEx.WindowResize(this.renderer, this.camera)
+  }
+
+  Environment.prototype.initStats = function () {
+    var self = this
+    this.stats = new Stats();
+    this.stats.setMode( 0 ); // 0: fps, 1: ms, 2: mb
+    // align top-left
+    this.stats.domElement.style.position = 'absolute';
+    this.stats.domElement.style.left = '0px';
+    this.stats.domElement.style.top = '0px';
+    document.body.appendChild( this.stats.domElement );
+    var $stats = $(this.stats.domElement)
+    $stats.hide()
+    this.onRenderFcts.push(function () {
+      self.stats.begin();
+      self.stats.end();
+    })
+
+    $(document).on('keypress', function (e) {
+      if ( e.keyCode === 115 || e.keyCode === 83) {
+        $stats.toggle()
+      }
+    })
+  }
+
+  Environment.prototype.initRendererStats = function  () {
+    var self = this
+    this.rendererStats   = new THREEx.RendererStats()
+
+    this.rendererStats.domElement.style.position = 'absolute'
+    this.rendererStats.domElement.style.right = '0px'
+    this.rendererStats.domElement.style.top   = '0px'
+    document.body.appendChild( this.rendererStats.domElement )
+
+    var $rendererStats = $(this.rendererStats.domElement)
+    $rendererStats.hide()
+
+    this.onRenderFcts.push(function () {
+      self.rendererStats.update(self.renderer);
+    })
+
+    $(document).on('keypress', function (e) {
+      if ( e.keyCode === 115 || e.keyCode === 83) {
+        $rendererStats.toggle()
+      }
+    })
+  }
+
+
+
   ///////////////////////////////////////////////////////////////////////////////////
   //////////////////////////////////// UTILITIES ////////////////////////////////////
   ///////////////////////////////////////////////////////////////////////////////////
 
   ///////////////////////// adding & removing objects from scene /////////////////////////////////
 
-  environment.addObjectsToScene = function (objects) {
-    _.forEach(objects, environment.addObjectToScene)
+  Environment.prototype.addObjectsToScene = function (objects) {
+    _.forEach(objects, this.addObjectToScene.bind(this))
   }
 
-  environment.addObjectToScene = function (object) {
+  Environment.prototype.addObjectToScene = function (object) {
     if (object.mesh) {
-      environment.scene.add(object.mesh)
+      this.scene.add(object.mesh)
     } else {
-      environment.scene.add(object)
+      this.scene.add(object)
     }
   }
 
-  environment.removeObjectFromScene = function (object) {
-    environment.scene.remove( object.mesh )
+  Environment.prototype.removeObjectFromScene = function (object) {
+    this.scene.remove( object.mesh )
   }
 
-  environment.removeObjectsFromScene = function (objects) { // duplicate of ebove function
-    _.forEach( objects, environment.removeObjectFromScene )
+  Environment.prototype.removeObjectsFromScene = function (objects) { // duplicate of ebove function
+    _.forEach( objects, this.removeObjectFromScene.bind(this) )
   }
 
   //////////////////////////////////// billboarding ////////////////////////////////////////////////
 
-  environment.billboardObjects = function (objects) {
-    _.forEach(objects, function(object) {
-      environment.billboardObject(object)
-    })
+  Environment.prototype.billboardObjects = function (objects) {
+    _.forEach(objects, this.billboardObject.bind(this))
   }
 
-  environment.billboardObject = function (object) {
-    object.mesh.quaternion.copy( environment.camera.quaternion )
+  Environment.prototype.billboardObject = function (object) {
+    object.mesh.quaternion.copy( this.camera.quaternion )
   }
+
+  var environment = new Environment()
 
   environment.init()
 
